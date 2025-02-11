cedf6fe5ce1c8266f0cdef8516efe17f5a1818e3
Max Cardenas <max.cardenas.cas@gmail.com>
2025-02-11 13:52:37 -0500

    chore: remove comments for adjusting size

--- src/core/components/cards/SkeletonTalentCard.tsx
+++ src/core/components/cards/SkeletonTalentCard.tsx
@@ -9,10 +9,10 @@ export const SkeletonCard = () => {
             {/* Contenido (skeleton) */}
             <div className="w-3/4 md:w-full space-y-2">
                 {/* Nombre y apellidos (skeleton) */}
-                <div className="h-4 bg-gray-300 rounded w-64"></div> {/* Ajusta el ancho según el contenido real */}
+                <div className="h-4 bg-gray-300 rounded w-64"></div>
 
                 {/* Puesto (skeleton) */}
-                <div className="h-4 bg-gray-300 rounded w-48"></div> {/* Ajusta el ancho según el contenido real */}
+                <div className="h-4 bg-gray-300 rounded w-48"></div>
 
                 {/* Estrellas (skeleton) */}
                 <div className="flex gap-2 my-2">
@@ -24,14 +24,14 @@ export const SkeletonCard = () => {
                 {/* Ubicación (skeleton) */}
                 <div className="flex items-center gap-2 my-1 lg:h-5">
                     <div className="h-5 w-5 bg-gray-300 rounded-full"></div>
-                    <div className="h-4 bg-gray-300 rounded w-32"></div> {/* Ajusta el ancho según el contenido real */}
+                    <div className="h-4 bg-gray-300 rounded w-32"></div>
                 </div>
 
                 {/* Montos (skeleton) */}
                 <div className="text-sm text-[#71717A]">
                     <div className="flex flex-row md:flex-col xl:flex-row gap-2 md:gap-0 xl:gap-2 flex-wrap">
-                        <div className="h-4 bg-gray-300 rounded w-48"></div> {/* Ajusta el ancho según el contenido real */}
-                        <div className="h-4 bg-gray-300 rounded w-48"></div> {/* Ajusta el ancho según el contenido real */}
+                        <div className="h-4 bg-gray-300 rounded w-48"></div>
+                        <div className="h-4 bg-gray-300 rounded w-48"></div>
                     </div>
                 </div>
             </div>
